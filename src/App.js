0089e01464c7174b116fdba72b33f057d737cd9c
CeViMu <vce02060@innovaccion.mx>
2022-07-31 23:14:27 -0500

    A cerca de propiedades

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React from "react";
 import logo from './logo.svg';
 import Componente from "./components/Componente";
-import './App.css';
+import Propiedades from "./components/Propiedades";
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor="nombre">Nombre</label>
-        <input type="text" id="nombre"></input>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <section>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <label htmlFor="nombre">Nombre</label>
+          <input type="text" id="nombre"></input>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+       <section>
         <Componente msg="Hola soy un componente desde una prop"></Componente> 
+        <hr></hr>
+        <Propiedades 
+        cadena="Esto es una cadena de texto" 
+        numero={19} 
+        booleano={true}
+        arreglo = {[1,2,3]}
+        objeto = {{nombre: "Cecy", correo: "cec@hotmail.com"}}
+        funcion={num => num*num}
+        elementoReact={<i>Esto es un elemento React</i>}
+        componenteReact={<Componente msg="Soy un componente pasado como prop"></Componente>}>
+        </Propiedades>
+
       </section>
+      </header>
     </div>
   );
 }
